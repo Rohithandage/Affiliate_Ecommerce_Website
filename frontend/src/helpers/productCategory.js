05e227803b5ca29cd4b039f3077cf67ad04adf9e
Rohit Handage <rohithandage2002@gmail.com>
2024-11-30 23:12:41 +0530

    changes

--- frontend/src/helpers/productCategory.js
+++ frontend/src/helpers/productCategory.js
@@ -1,18 +1,9 @@
 const productCategory = [
-<<<<<<< HEAD
     { id : 1, label : "Airpodes", value : "airpodes"},
     { id : 2, label : "Phone covers", value : "Phone covers"},
     { id : 3, label : "Earphones", value : "earphones"},
     { id : 4, label : "Mobiles", value : "mobiles"},
-    { id : 5, label : "Watches", value : "watches"},
-=======
-     { id : 1, label : "Phone Covers", value : "Phone Covers"},
-    { id : 2, label : "Airpodes", value : "airpodes"},
-    { id : 3, label : "Earphones", value : "earphones"},
-     { id : 4, label : "Watches", value : "watches"},
-    { id : 5, label : "Mobiles", value : "mobiles"},
-   
->>>>>>> 9e5b25d31274d74f22af799b6b0c15dc51a1e4bc
+{ id : 5, label : "Watches", value : "watches"},
 ]
 
 

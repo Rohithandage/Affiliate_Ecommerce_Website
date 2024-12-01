06f98c8b490b9de6dc9c480d9801be6ff83e269d
Rohit Handage <rohithandage2002@gmail.com>
2024-12-01 22:55:35 +0530

    changes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
             {
                 path : "search",
                 element : <SearchProduct/>
-            },
+            }
         ]
     }
 ])
